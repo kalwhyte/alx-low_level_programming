1e4b2eee37e309d458f62a0e55b7dec988d9a658
kalwhyte <superkalel55@gmail.com>
2022-10-13 20:35:31 -0700

    pushing task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- *  main - Entry point
+ * main - Entry point
  *
- *  Description: a C program that prints exactly with 'proper...'
+ * Description: a C program that prints exactly with 'proper...'
  *
- *  Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  *
  */
 
