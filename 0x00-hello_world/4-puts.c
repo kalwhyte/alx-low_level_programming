f8fa87ba8801b6ebadf2083cfb31ff9af8f2f08d
kalwhyte <superkalel55@gmail.com>
2022-10-13 19:34:56 -0700

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	/*inputing the output*/
+	char myVar[] = "\"Programming is like building a multilingual puzzle";
+	/*using puts to output myVar*/
+	puts(myVar);
+
+	return (0);
+}	
