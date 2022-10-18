edaed60d292032936dcba94e9e323bd91e661e4b
kalwhyte <superkalel55@gmail.com>
2022-10-18 11:11:07 -0700

    task 3

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,17 +1,1 @@
-#include "main.h"
-
-/**
- * _isalpha - a function that checks for alphabetic character
- *
- * @c: single letter input
- *
- * Return: if c is upper or lower letter 1 else 0
- */
-
-int _isalpha(int c)
-{
-	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
-		return (1);
-	else
-		return (0);
-}
+typedef int make_iso_compiler_happy;
