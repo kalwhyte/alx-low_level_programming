2117b804c4caf418576f7e5dc51b207fd44c1057
kalwhyte <superkalel55@gmail.com>
2022-10-18 11:08:01 -0700

    task 3

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,15 +1,1 @@
-#include "main.h"
-
-/**
- * _islower - checks for lowercase character
- * @c: Single letter input
- * Return: Always 0 if successful
- */
-
-int _islower(int c)
-{
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
-}
+typedef int make_iso_compiler_happy;
