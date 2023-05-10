e879b4c4da0902a25aad03b2519a6dd2671a97aa
kalwhyte <superkalel55@gmail.com>
2023-05-10 19:11:09 +0000

    A function that searches for a value in a sorted array of integers

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,61 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary_recursive - Searches recursively for a value in a sorted
+ *                             array of integers using binary search.
+ * @array: A pointer to the first element of the [sub]array to search.
+ * @left: The starting index of the sub-array to search.
+ * @right: The ending index of the sub-array to search.
+ * @value: The value to search for.
+ *
+ * Return: If the value is not present, -1.
+ *         Otherwise, the index where the value is located.
+ *
+ * Description: Prints the sub-array being searched after each change.
+ */
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
+{
+	size_t whyte;
+
+	if (array == NULL)
+		return (-1);
+
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (whyte = left; whyte < right; whyte++)
+			printf("%d, ", array[whyte]);
+		printf("%d\n", array[whyte]);
+
+		whyte = left + (right - left) / 2;
+
+		if (array[whyte] == value && (whyte == left || array[whyte - 1] != value))
+			return (whyte);
+		else if (array[whyte] >= value)
+			right = whyte;
+		else
+			left = whyte + 1;
+    }
+
+    return (-1);
+}
+
+/**
+ * advanced_binary - Searches for a value in a sorted array
+ *                   of integers using advanced binary search.
+ * @array: A pointer to the first element of the array to search.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: If the value is not present or the array is NULL, -1.
+ *         Otherwise, the first index where the value is located.
+ *
+ * Description: Prints the [sub]array being searched after each change.
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+    if (array == NULL || size == 0)
+        return (-1);
+
+    return (advanced_binary_recursive(array, 0, size - 1, value));
+}
